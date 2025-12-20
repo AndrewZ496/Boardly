761cbb1651a642d6df5fd9096ef8c907f486c2f7
Andrew Zhao <andrewzhao5620@gmail.com>
2025-12-19 23:50:23 -0500

    Fixing production favorite and searching

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { use } from "react";
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
 import { BoardList } from "./_components/board-list";
@@ -17,7 +16,11 @@ const DashboardPage = ({ searchParams }: DashboardPageProps) => {
 
   return (
     <div className="flex-1 h-[calc(100%-80px)] p-6">
-      {!organization ? <EmptyOrg /> : <BoardList orgId={organization.id} />}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} searchParams={searchParams} />
+      )}
     </div>
   );
 };
